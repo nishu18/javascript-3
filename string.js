1d68b3f9af441213bd4ec1d2b992352c5dbdaf34
nishu18 <99903771+nishu18@users.noreply.github.com>
2022-11-20 00:39:05 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,26 @@
+// problem 1
+
+let str= "nishu\"";
+console.log(str.length)
+
+// problem 2
+
+const sentense="Try the new cross-platform PowerShell";
+const word='new';
+console.log(sentense.includes(word))
+
+//problem 3
+
+const sentens="Try the new cross-platform PowerShell";
+console.log(sentens.toLowerCase())
+
+//problem 4
+
+let strr= "please give rs 1000"
+let amount =Number.parseInt(strr.slice(15))
+console.log(amount)
+
+//problem 5
+let friend="nihal";
+friend[2]="j"
+console.log(friend) // friend is  not changed, becouse string is imutable 
